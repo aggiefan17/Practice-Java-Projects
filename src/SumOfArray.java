742c1e26329d7b5cfaea986e3fe857966e3ef188
terryhale <aggiefan17@yahoo.com>
2017-12-23 17:21:39 -0600

    finished sum array practice

--- src/SumOfArray.java
+++ src/SumOfArray.java
@@ -1,11 +1,36 @@
+import java.util.Scanner;
+
 class SumOfArray {
+
     public static void main(String[] args) {
         int[] array = {10, 20, 30, 40, 50, 10};
         int sum = 0;
 
         for (int num: array) {
-            sum = sum + num
+            sum = sum + num;
         }
-        System.out.println(sum);
+        System.out.println("The sum or the array si: " + sum);
     }
+}
+
+class SumDemo {
+
+    public static void main(String[] args) {
+
+
+    Scanner in = new Scanner(System.in);
+
+    int[] numArray = new int[10];
+    int total = 0;
+
+        System.out.println("Enter 10 numbers: ");
+        for (int i = 0; i < 10; i++) {
+        numArray[i] = in.nextInt();
+    }
+        for(int number : numArray) {
+        total = total + number;
+    }
+        System.out.println("The sum of the new array is: " + total);
+    }
+
 }
