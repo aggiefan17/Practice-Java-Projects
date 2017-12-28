6d92e75a67d607ae6f15e688c83dc6b5be7ef2be
terryhale <aggiefan17@yahoo.com>
2017-12-28 13:54:53 -0600

    completed practice on inheritance

---
+++ src/Animal.java
@@ -0,0 +1,32 @@
+
+public class Animal {
+
+    private int age;
+
+    public Animal(int age) {
+        this.age = age;
+        System.out.println("An animal has been created!");
+    }
+
+    public void eat() {
+        System.out.println("An animal is eating");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public static void main(String[] args) {
+
+        Animal a = new Animal(5);
+        Dog d = new Dog();
+        Cat c = new Cat();
+        d.ruff();
+        c.meow();
+        a.eat();
+        d.eat();
+        c.eat();
+        d.run();
+        c.prance();
+    }
+}
